b222e249aa3d4679a2a17e563de0fd352369792e
Renan Gonçalves <renan.saddam@gmail.com>
2014-05-12 22:49:53 +0200

    Creating a gulp task for running the tests.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,37 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
+var karma = require('gulp-karma');
 
-gulp.task('default', ['coffee']);
+gulp.task('default', ['js-src']);
 
-gulp.task('coffee', function() {
-  return gulp.src('src/*.coffee', { read: false })
+var coffee = function(dir) {
+  return gulp.src(dir + '/*.coffee', { read: false })
     .pipe(browserify({
       transform: ['coffeeify'],
-      extensions: ['.coffee']
+      extensions: ['.coffee'],
+      debug: true
     }))
-    .pipe(rename('src.js'))
+    .pipe(rename(dir + '.js'))
     .pipe(gulp.dest('./build/js'));
+};
+
+gulp.task('js-src', function() {
+  return coffee('src');
+});
+
+gulp.task('js-test', function() {
+  return coffee('test');
+});
+
+gulp.task('test', ['default', 'js-test'], function() {
+  return gulp.src('build/js/*.js')
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
 });
