6fce93e06b943eb2c7fcfdd6cfd906664be7a182
Renan Gonçalves <renan.saddam@gmail.com>
2015-10-14 00:08:41 +0200

    Add polyfill for generators.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 /* global document */
 
+import 'babel-core/polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import GenerationView from './src/components/GenerationView';
 
 ReactDOM.render(
